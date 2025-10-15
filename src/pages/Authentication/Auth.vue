25d60aa0c9bbdc8c08586a5179f8bc2fd14d5c10
GABRIEL EHRAT FAGUNDES <gabriel_e_fagundes@estudante.sesisenai.org.br>
2025-10-15 16:54:14 -0300

    :sparkles: feat: Auth page completed

--- src/pages/Authentication/Auth.vue
+++ src/pages/Authentication/Auth.vue
@@ -1,17 +1,39 @@
 <script setup>
-  import Form from '../../widgets/Form.vue'
+  import{ ref } from 'vue'
+  import Login from '../../widgets/Login.vue';
+  import Signin from '../../widgets/Signin.vue';
+
+  const tabs = {
+    Login,
+    Signin
+  }
+  const currTab = ref('Login');
 </script>
 
 <template>
     <div class="flex flex-col items-center my-[6rem]">
         <p>Welcome to</p>
         <h1 class="text-6xl">Feely<span class="logoColor">tics</span></h1>
-    </div>
-    <div>
-        <Form title="Log into your account" />
+        <div class="mt-[2rem] flex gap-4">
+            <button v-for="(_,tab) in tabs" :key="tab" :class="['tab-button', { active: currTab === tab }]" @click="currTab = tab">{{ tab }}</button>
+        </div>
+        <keep-alive>
+          <component :is="tabs[currTab]"></component>
+        </keep-alive>
     </div>
 </template>
 
-<style>
+<style scoped>
+button{
+  cursor: pointer;
+  border-top: 2px solid #181818;
+  border-top-left-radius: 2rem;
+  border-top-right-radius: 2rem;
+  padding: 0.5rem 2rem;
+  transition: all 0.3s ease-in-out;
+}
 
+button:hover{
+    transform: translateY(-0.2rem);
+}
 </style>
---
+++ src/widgets/Login.vue
@@ -0,0 +1,82 @@
+<script>
+export default{
+  name: 'Login',
+}
+</script>
+
+<template>
+    <div class="flex flex-col items-center tab">
+        <div class="boxShadow px-[10rem] py-[2.5rem] rounded-2xl text-center">
+            <h3 class="text-xl">Log into your account</h3>
+            <div class="mt-[2rem]">
+              <div>
+                <input type="email" v-model="email" placeholder="Email..." class="field mb-[2rem]" />
+              </div>
+              <div>
+                <input type="password" v-model="passcode" placeholder="Password..." class="field mb-[2rem]" />
+              </div>
+              <div>
+                <button @click="login" class="submit">Log in!</button>
+              </div>
+              <a href="/home" class="text-xs">↩ Back to Home</a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+a{
+  color: #535353;
+  transition: color 0.3s ease-in-out;
+}
+
+a:hover{
+  color: #5B94FF;
+}
+
+.boxShadow{
+    box-shadow: rgba(255, 255, 255, 0.25) 0px 1px 70px -12px inset, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px inset;
+}
+
+.field{
+    background: #000;
+    border-bottom: 2px solid #181818;
+    padding: 10px;
+    width: 100%;
+    color: white;
+    font-size: 0.8rem;
+    border-radius: 2rem;
+}
+
+.field:focus{
+  background: 
+    linear-gradient(#000 0 0) padding-box,
+    linear-gradient(to right, #8fb6ff, #3179ff) border-box;
+  color: #313149;
+  padding: 10px;
+  border-bottom: 2px solid transparent;
+  border-radius: 2rem;
+  outline: none;
+  color: white;
+  transition: all 0.3s ease-in-out;
+}
+
+.submit{
+  cursor: pointer;
+  padding: 0.6rem 2rem;
+  border-radius: 2rem;
+  border-bottom: 2px solid #181818;
+}
+
+.submit:hover{
+  background: 
+    linear-gradient(#000 0 0) padding-box,
+    linear-gradient(to right, #8fb6ff, #3179ff) border-box;
+  color: #313149;
+  border-bottom: 2px solid transparent;
+  border-radius: 2rem;
+  outline: none;
+  color: white;
+  transition: all 0.3s ease-in-out;
+}
+</style>
