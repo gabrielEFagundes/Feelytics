db58b17b5adb5bc5663d42a4779c2970b32ad0d9
GABRIEL EHRAT FAGUNDES <gabriel_e_fagundes@estudante.sesisenai.org.br>
2025-10-27 21:35:48 -0300

    :sparkles: feat: Search is being send to python backend

--- src/utils/data-analysis/routes.py
+++ src/utils/data-analysis/routes.py
@@ -1,17 +1,19 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_restx import Api, Resource
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 
 from reddit.dataCollection import getRedditData, turnDataIntoJson
 
 app = Flask(__name__)
 api = Api(app)
-CORS(app)
+CORS(app, resources={r'/*': {"origins": "http://localhost:3000"}})
 
+@cross_origin()
 @api.route('/api/search', methods=['GET', 'POST'])
 class SearchResource(Resource):
-    def get(self):
-        return # TODO: find a way to return just the search value from frontend
+    def post(self):
+        data = request.get_json()
+        return data
 
 @api.route('/api/data/reddit', methods=['GET'])
 class RedditDataResource(Resource):
@@ -19,4 +21,4 @@ class RedditDataResource(Resource):
         return turnDataIntoJson(getRedditData())
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=5000)
