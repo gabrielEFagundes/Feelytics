1bc07454f12366e7b7ef0d711186c2d81f5ac1d2
Gabriel Ehrat <gabriel_e_fagundes@estudante.sesisenai.org.br>
2025-11-03 21:47:18 -0300

    :adhesive_bandage: fix: Removing useless export

--- src/pages/Dashboards/dashboardComponents.js
+++ src/pages/Dashboards/dashboardComponents.js
@@ -3,6 +3,4 @@ export { default as PostsBoard } from '../../widgets/Databoards/PostsBoard.vue';
 export { default as AiResume } from '../../widgets/Databoards/AiResume.vue';
 export { default as SearchBar } from '../../widgets/SearchBar.vue';
 
-export { default as finalResults } from '../../widgets/SearchBar.vue';
-
 // just so we don't have to keep adding lines on the <script setup>
